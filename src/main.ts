aff97443a28c6aa9a81cca8624c726498982a72a
Tibo Delor <delor.thibault@gmail.com>
2022-05-24 16:38:22 +1000

    feat: Support appending to env file

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import {
   GetSecretValueCommand,
   SecretsManagerClient
 } from '@aws-sdk/client-secrets-manager'
+import {promises as fs} from 'fs'
 
 function getOptionalInput(input: string): string | undefined {
   const result = core.getInput(input, {required: false, trimWhitespace: true})
@@ -12,33 +13,50 @@ function getOptionalInput(input: string): string | undefined {
   return result
 }
 
-function processJson(
+async function processJson(
   secret: string,
   maskJsonValues: boolean,
   keysAsEnvVars: boolean,
-  keysAsOutputs: boolean
-): void {
+  keysAsOutputs: boolean,
+  envFilePath?: string
+): Promise<void> {
   if (!maskJsonValues && !keysAsEnvVars && !keysAsOutputs) {
     core.debug('No JSON processing needed')
     return
   }
 
   core.debug('Parsing JSON')
-  try {
-    const secretObj = JSON.parse(secret, (_, value) => {
-      if (maskJsonValues) core.setSecret(value)
-      return value
-    })
+  const secretObj = JSON.parse(secret, (_, value) => {
+    if (maskJsonValues) core.setSecret(value)
+    return value
+  })
 
-    if (typeof secretObj !== 'object') {
-      throw new Error('Secret was JSON but not an object 🤔')
-    }
-
-    exportKeys(secretObj, keysAsEnvVars, keysAsOutputs)
-  } catch (error) {
-    core.warning('Secret wasnt json')
-    if (error instanceof Error) core.warning(error.message)
+  if (typeof secretObj !== 'object') {
+    throw new Error('Secret was JSON but not an object 🤔')
   }
+
+  exportKeys(secretObj, keysAsEnvVars, keysAsOutputs)
+  writeEnvFile(secretObj, envFilePath)
+}
+
+async function writeEnvFile(
+  secretObj: Object,
+  envFilePath?: string
+): Promise<void> {
+  if (!envFilePath) {
+    core.debug('No need to export env file')
+    return
+  }
+  await fs.appendFile(
+    envFilePath,
+    `
+
+#### Loaded via t-botz/aws-secrets-manager-read-action
+${Object.entries(secretObj)
+  .map(e => `${e[0]}=${e[1]}`)
+  .join('\n')}
+`
+  )
 }
 
 function exportKeys(
@@ -77,6 +95,7 @@ async function run(): Promise<void> {
     const maskJsonValues = core.getBooleanInput('mask-json-values')
     const keysAsEnvVars = core.getBooleanInput('keys-as-env-vars')
     const keysAsOutputs = core.getBooleanInput('keys-as-outputs')
+    const envFilePath = getOptionalInput('append-to-env-file')
 
     // Used mainly for unit test to use localstack
     const awsEndpoint = process.env['AWS_SECRETS_MANAGER_ENDPOINT_URL']
@@ -97,19 +116,20 @@ async function run(): Promise<void> {
 
     if (response.SecretString) {
       if (maskValue) core.setSecret(response.SecretString)
-      processJson(
+      await processJson(
         response.SecretString,
         maskJsonValues,
         keysAsEnvVars,
-        keysAsOutputs
+        keysAsOutputs,
+        envFilePath
       )
     } else {
       core.debug('SecretString is undefined')
     }
     core.setOutput('secret', response.SecretString)
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
-    else core.setFailed('Unknow Error ?!')
+    if (error instanceof Error) core.setFailed(error)
+    else core.setFailed(`Unexpected error: ${error}`)
   }
 }
 
